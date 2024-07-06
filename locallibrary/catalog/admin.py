5b5ce8da048ef102c091a1efbcb53bf2b60fefa4
Anononmos <ainan.kashif@gmail.com>
2024-07-06 15:48:59 -0400

    admin models

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,8 +3,20 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.site.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
